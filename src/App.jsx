fb9afd8f6f4cf0b4fb3230a21c111ed26a98bd16
skancher <sohank@gmail.com>
2024-02-22 20:54:25 -0800

    Add color pallete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <NavigationBar />
+      <Routes>
+      </Routes>
+    </Router>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ export default {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: {
+          '50': '#edfaff',
+          '100': '#d6f3ff',
+          '200': '#b5ecff',
+          '300': '#83e2ff',
+          '400': '#48cfff',
+          '500': '#1eb1ff',
+          '600': '#0694ff',
+          '700': '#0080ff',
+          '800': '#0861c5',
+          '900': '#0d549b',
+          '950': '#0e335d',
+        },
+      }
     },
   },
   plugins: [],
