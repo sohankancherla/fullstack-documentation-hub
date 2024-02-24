1500c2ce7082c71b2bd6faeb03ddb6fe9cc314cc
skancher <sohank@gmail.com>
2024-02-24 12:55:00 -0800

    Change panel to side overlay

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,6 @@
-import { Disclosure, Dialog, Menu, Transition } from '@headlessui/react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import { Bars3Icon, SunIcon, MoonIcon, ComputerDesktopIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, SunIcon, MoonIcon, ComputerDesktopIcon, XMarkIcon, Cog6ToothIcon, HomeIcon, UsersIcon, FolderIcon, DocumentDuplicateIcon, CalendarIcon, ChartPieIcon } from '@heroicons/react/24/outline'
 import React, { useEffect, useState, Fragment } from 'react';
 
 import logo from "../assets/logo/logo-transparent.png"
@@ -8,6 +8,24 @@ import logo from "../assets/logo/logo-transparent.png"
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+const navigation = [
+  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
+  { name: 'Team', href: '#', icon: UsersIcon, current: false },
+  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
+  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+]
+const teams = [
+  { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
+  { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
+  { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+]
+const userNavigation = [
+  { name: 'Your profile', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
 
 export default function NavigationBar() {
 
@@ -43,10 +61,133 @@ export default function NavigationBar() {
     setTheme(newTheme);
     window.localStorage.setItem('theme', newTheme);
   };
-  console.log(currentTheme)
+
+  const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
-        <>
+    <>
+      <div>
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  {/* Sidebar component, swap this element with another sidebar if you like */}
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+                    <div className="flex h-16 shrink-0 items-center">
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=white"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <nav className="flex flex-1 flex-col">
+                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                        <li>
+                          <ul role="list" className="-mx-2 space-y-1">
+                            {navigation.map((item) => (
+                              <li key={item.name}>
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    item.current
+                                      ? 'bg-indigo-700 text-white'
+                                      : 'text-indigo-200 hover:text-white hover:bg-indigo-700',
+                                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                  )}
+                                >
+                                  <item.icon
+                                    className={classNames(
+                                      item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white',
+                                      'h-6 w-6 shrink-0'
+                                    )}
+                                    aria-hidden="true"
+                                  />
+                                  {item.name}
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                        <li>
+                          <div className="text-xs font-semibold leading-6 text-indigo-200">Your teams</div>
+                          <ul role="list" className="-mx-2 mt-2 space-y-1">
+                            {teams.map((team) => (
+                              <li key={team.name}>
+                                <a
+                                  href={team.href}
+                                  className={classNames(
+                                    team.current
+                                      ? 'bg-indigo-700 text-white'
+                                      : 'text-indigo-200 hover:text-white hover:bg-indigo-700',
+                                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                  )}
+                                >
+                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
+                                    {team.initial}
+                                  </span>
+                                  <span className="truncate">{team.name}</span>
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                        <li className="mt-auto">
+                          <a
+                            href="#"
+                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
+                          >
+                            <Cog6ToothIcon
+                              className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
+                              aria-hidden="true"
+                            />
+                            Settings
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
         <nav className="shadow dark:shadow-slate-950">
           <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8 ">
             <div className="flex h-16 justify-between">
@@ -108,14 +249,9 @@ export default function NavigationBar() {
               </div>
               <div className="flex items-center lg:hidden">
                 {/* Mobile menu button */}
-                <button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 dark:text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-700">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
+                <button type="button" className="-m-2.5 p-2.5 text-gray-400 lg:hidden" onClick={() => setSidebarOpen(true)}>
+                  <span className="sr-only">Open sidebar</span>
+                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                 </button>
               </div>
               <div className="hidden lg:ml-4 lg:flex lg:items-center">
@@ -125,9 +261,9 @@ export default function NavigationBar() {
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Dark Mode</span>
                     {(currentTheme === "light" || currentTheme === "system-light") ? (
-                      <SunIcon className={classNames(currentTheme === "light" ? "stroke-primary-700" : "stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true" />
+                      <SunIcon className={classNames(currentTheme === "light" ? "stroke-primary-700" : "stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true" />
                     ) : (
-                      <MoonIcon className={classNames(currentTheme === "dark" ? "stroke-primary-700" : "stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true" />
+                      <MoonIcon className={classNames(currentTheme === "dark" ? "stroke-primary-700" : "stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true" />
                     )}
                   </Menu.Button>
                   </div>
@@ -181,6 +317,7 @@ export default function NavigationBar() {
             </div>
           </div>
         </nav>
-        </>
+      </div>
+    </>
   )
 }
