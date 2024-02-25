7b7bb4de0d8af1624387c609a95908398ef23845
skancher <sohank@gmail.com>
2024-02-24 21:41:10 -0800

    Change dark color on dark mode

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -47,7 +47,7 @@ export default function NavigationBar({navigation, logo}) {
 
   return (
     <>
-      <div className="antialiased min-h-screen bg-white dark:bg-slate-900 text-slate-700 dark:text-slate-400">
+      <div className="antialiased min-h-screen bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-400">
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
             <Transition.Child
@@ -66,11 +66,11 @@ export default function NavigationBar({navigation, logo}) {
               <Transition.Child
                 as={Fragment}
                 enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
+                enterFrom="-trangray-x-full"
+                enterTo="trangray-x-0"
                 leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
+                leaveFrom="trangray-x-0"
+                leaveTo="-trangray-x-full"
               >
                 <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
                   <Transition.Child
@@ -89,7 +89,7 @@ export default function NavigationBar({navigation, logo}) {
                       </button>
                     </div>
                   </Transition.Child>
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white dark:bg-slate-800 px-6 pb-4">
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white dark:bg-gray-800 px-6 pb-4">
                     <div className="flex h-16 shrink-0 items-center">
                       <a href="/">
                         <img
@@ -107,10 +107,10 @@ export default function NavigationBar({navigation, logo}) {
                               <li key={item.name}>
                                 <a
                                   href={item.href}
-                                  className="text-slate-700 dark:text-slate-200 hover:text-white dark:hover:text-white hover:bg-primary-700 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                  className="text-gray-700 dark:text-gray-200 hover:text-white dark:hover:text-white hover:bg-primary-700 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                 >
                                   <item.icon
-                                    className="text-slate-700 dark:text-slate-200 group-hover:text-white dark:group-hover:text-white hover:bg-primary-700 h-6 w-6 shrink-0"
+                                    className="text-gray-700 dark:text-gray-200 group-hover:text-white dark:group-hover:text-white hover:bg-primary-700 h-6 w-6 shrink-0"
                                     aria-hidden="true"
                                   />
                                   {item.name}
@@ -121,15 +121,15 @@ export default function NavigationBar({navigation, logo}) {
                         </li>
                         <li className="mt-auto">
                           <Menu as="div" className="relative -mx-2">
-                            <Menu.Button className="text-slate-700 dark:text-slate-200 hover:text-white dark:hover:text-white hover:bg-primary-700 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full">
+                            <Menu.Button className="text-gray-700 dark:text-gray-200 hover:text-white dark:hover:text-white hover:bg-primary-700 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full">
                               <span className="absolute -inset-1.5" />
                               <span className="sr-only">Dark Mode</span>
                               {(currentTheme === "light") ? (
-                                <SunIcon className="stroke-slate-700 dark:stroke-slate-200 group-hover:stroke-white dark:group-hover:stoke-white h-6 w-6 shrink-0" aria-hidden="true" />
+                                <SunIcon className="stroke-gray-700 dark:stroke-gray-200 group-hover:stroke-white dark:group-hover:stoke-white h-6 w-6 shrink-0" aria-hidden="true" />
                               ) : ((currentTheme === "dark") ? 
-                              (<MoonIcon className="stroke-slate-700 dark:stroke-slate-200 group-hover:stroke-white dark:group-hover:stoke-white h-6 w-6 shrink-0" aria-hidden="true" />
+                              (<MoonIcon className="stroke-gray-700 dark:stroke-gray-200 group-hover:stroke-white dark:group-hover:stoke-white h-6 w-6 shrink-0" aria-hidden="true" />
                               ) : (
-                                <ComputerDesktopIcon className="stroke-slate-700 dark:stroke-slate-200 group-hover:stroke-white dark:group-hover:stoke-white h-6 w-6 shrink-0" aria-hidden="true" />
+                                <ComputerDesktopIcon className="stroke-gray-700 dark:stroke-gray-200 group-hover:stroke-white dark:group-hover:stoke-white h-6 w-6 shrink-0" aria-hidden="true" />
                               ))}
                               Appearance
                             </Menu.Button>
@@ -142,14 +142,14 @@ export default function NavigationBar({navigation, logo}) {
                               leaveFrom="transform opacity-100 scale-100"
                               leaveTo="transform opacity-0 scale-95"
                             >
-                              <Menu.Items className="absolute right-0 bottom-12 z-10 mt-2 w-32 origin-top-right rounded-md bg-white dark:bg-slate-800  py-1 shadow-lg dark:shadow-slate-900 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                              <Menu.Items className="absolute right-0 bottom-12 z-10 mt-2 w-32 origin-top-right rounded-md bg-white dark:bg-gray-800  py-1 shadow-lg dark:shadow-gray-900 ring-1 ring-black ring-opacity-5 focus:outline-none">
                                 <Menu.Item>
                                   {({ active }) => (
                                     <a
                                       onClick={() => handleSetTheme('light')}
-                                      className={classNames(active ? 'bg-gray-100 dark:bg-slate-700' : '', theme === 'light' ? 'text-primary-700' : 'text-slate-700 dark:text-slate-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
+                                      className={classNames(active ? 'bg-gray-100 dark:bg-gray-700' : '', theme === 'light' ? 'text-primary-700' : 'text-gray-700 dark:text-gray-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
                                     >
-                                      <SunIcon className={classNames(theme === "light" ? "stroke-primary-700" : "stroke-slate-700 dark:stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
+                                      <SunIcon className={classNames(theme === "light" ? "stroke-primary-700" : "stroke-gray-700 dark:stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
                                       Light
                                     </a>
                                   )}
@@ -158,9 +158,9 @@ export default function NavigationBar({navigation, logo}) {
                                   {({ active }) => (
                                     <a
                                       onClick={() => handleSetTheme('dark')}
-                                      className={classNames(active ? 'bg-gray-100 dark:bg-slate-700' : '', theme === 'dark' ? 'text-primary-700' : 'text-slate-700 dark:text-slate-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
+                                      className={classNames(active ? 'bg-gray-100 dark:bg-gray-700' : '', theme === 'dark' ? 'text-primary-700' : 'text-gray-700 dark:text-gray-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
                                     >
-                                      <MoonIcon className={classNames(theme === "dark" ? "stroke-primary-700" : "stroke-slate-700 dark:stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
+                                      <MoonIcon className={classNames(theme === "dark" ? "stroke-primary-700" : "stroke-gray-700 dark:stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
                                       Dark
                                     </a>
                                   )}
@@ -169,9 +169,9 @@ export default function NavigationBar({navigation, logo}) {
                                   {({ active }) => (
                                     <a
                                       onClick={() => handleSetTheme('system')}
-                                      className={classNames(active ? 'bg-gray-100 dark:bg-slate-700' : '', theme === 'system' ? 'text-primary-700' : 'text-slate-700 dark:text-slate-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
+                                      className={classNames(active ? 'bg-gray-100 dark:bg-gray-700' : '', theme === 'system' ? 'text-primary-700' : 'text-gray-700 dark:text-gray-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
                                     >
-                                      <ComputerDesktopIcon className={classNames(theme === "system" ? "stroke-primary-700" : "stroke-slate-700 dark:stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
+                                      <ComputerDesktopIcon className={classNames(theme === "system" ? "stroke-primary-700" : "stroke-gray-700 dark:stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
                                       System
                                     </a>
                                   )}
@@ -188,7 +188,7 @@ export default function NavigationBar({navigation, logo}) {
             </div>
           </Dialog>
         </Transition.Root>
-        <nav className="shadow dark:shadow-slate-950">
+        <nav className="shadow dark:shadow-gray-950">
           <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8 ">
             <div className="flex h-16 justify-between">
               <div className="flex px-2 lg:px-0">
@@ -204,7 +204,7 @@ export default function NavigationBar({navigation, logo}) {
                 <div className="hidden lg:ml-10 lg:flex lg:space-x-8">
                   {navigation.map((item) => (
                     <Popover className="relative mt-4">
-                      <Popover.Button className="inline-flex items-center gap-x-0 text-sm font-semibold leading-6 text-slate-700 hover:text-primary-700 dark:text-slate-200 dark:hover:text-primary-700 focus:outline-none">
+                      <Popover.Button className="inline-flex items-center gap-x-0 text-sm font-semibold leading-6 text-gray-700 hover:text-primary-700 dark:text-gray-200 dark:hover:text-primary-700 focus:outline-none">
                         <span
                           className="px-1 pt-1 text-sm leading-6 font-semibold cursor-pointer"
                         >
@@ -216,35 +216,35 @@ export default function NavigationBar({navigation, logo}) {
                       <Transition
                         as={Fragment}
                         enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
+                        enterFrom="opacity-0 trangray-y-1"
+                        enterTo="opacity-100 trangray-y-0"
                         leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
+                        leaveFrom="opacity-100 trangray-y-0"
+                        leaveTo="opacity-0 trangray-y-1"
                       >
-                        <Popover.Panel className="absolute -left-32 2xl:left-1/2 z-10 mt-6 flex w-screen max-w-max 2xl:-translate-x-1/2 px-4">
-                          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white dark:bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 dark:ring-gray-950">
+                        <Popover.Panel className="absolute -left-32 2xl:left-1/2 z-10 mt-6 flex w-screen max-w-max 2xl:-trangray-x-1/2 px-4">
+                          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white dark:bg-gray-800 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 dark:ring-gray-950">
                             <div className="p-4">
                               {item.subpages.map((page) => (
-                                <div key={page.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50 dark:hover:bg-slate-700">
-                                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 dark:bg-slate-700 group-hover:bg-white dark:group-hover:bg-slate-800">
-                                    <page.icon className="h-6 w-6 text-slate-700 dark:text-slate-400 group-hover:text-primary-700" aria-hidden="true" />
+                                <div key={page.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-700 group-hover:bg-white dark:group-hover:bg-gray-800">
+                                    <page.icon className="h-6 w-6 text-gray-700 dark:text-gray-400 group-hover:text-primary-700" aria-hidden="true" />
                                   </div>
                                   <div>
-                                    <a href={page.href} className="font-semibold text-slate-900 dark:text-slate-200">
+                                    <a href={page.href} className="font-semibold text-gray-900 dark:text-gray-200">
                                       {page.name}
                                       <span className="absolute inset-0" />
                                     </a>
-                                    <p className="mt-1 text-slate-700 dark:text-slate-400">{page.description}</p>
+                                    <p className="mt-1 text-gray-700 dark:text-gray-400">{page.description}</p>
                                   </div>
                                 </div>
                               ))}
                             </div>
-                            <div className="grid grid-cols-1 divide-x divide-gray-900/5 dark:divide-slate-800 bg-gray-50 dark:bg-slate-700">
+                            <div className="grid grid-cols-1 divide-x divide-gray-900/5 dark:divide-gray-800 bg-gray-50 dark:bg-gray-700">
                               <a
                                 key={item.name}
                                 href={item.href}
-                                className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-slate-900 dark:text-slate-200 hover:bg-gray-100 dark:hover:bg-slate-600"
+                                className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
                               >
                                 View More
                               </a>
@@ -304,14 +304,14 @@ export default function NavigationBar({navigation, logo}) {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 2xl:left-1/2 2xl:-translate-x-1/2 z-10 mt-2 w-32 origin-top-right rounded-md bg-white dark:bg-slate-800  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 2xl:left-1/2 2xl:-trangray-x-1/2 z-10 mt-2 w-32 origin-top-right rounded-md bg-white dark:bg-gray-800  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
                             onClick={() => handleSetTheme('light')}
-                            className={classNames(active ? 'bg-gray-100 dark:bg-slate-700' : '', theme === 'light' ? 'text-primary-700' : 'text-slate-700 dark:text-slate-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
+                            className={classNames(active ? 'bg-gray-100 dark:bg-gray-700' : '', theme === 'light' ? 'text-primary-700' : 'text-gray-700 dark:text-gray-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
                           >
-                            <SunIcon className={classNames(theme === "light" ? "stroke-primary-700" : "stroke-slate-700 dark:stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
+                            <SunIcon className={classNames(theme === "light" ? "stroke-primary-700" : "stroke-gray-700 dark:stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
                             Light
                           </a>
                         )}
@@ -320,9 +320,9 @@ export default function NavigationBar({navigation, logo}) {
                         {({ active }) => (
                           <a
                             onClick={() => handleSetTheme('dark')}
-                            className={classNames(active ? 'bg-gray-100 dark:bg-slate-700' : '', theme === 'dark' ? 'text-primary-700' : 'text-slate-700 dark:text-slate-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
+                            className={classNames(active ? 'bg-gray-100 dark:bg-gray-700' : '', theme === 'dark' ? 'text-primary-700' : 'text-gray-700 dark:text-gray-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
                           >
-                            <MoonIcon className={classNames(theme === "dark" ? "stroke-primary-700" : "stroke-slate-700 dark:stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
+                            <MoonIcon className={classNames(theme === "dark" ? "stroke-primary-700" : "stroke-gray-700 dark:stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
                             Dark
                           </a>
                         )}
@@ -331,9 +331,9 @@ export default function NavigationBar({navigation, logo}) {
                         {({ active }) => (
                           <a
                             onClick={() => handleSetTheme('system')}
-                            className={classNames(active ? 'bg-gray-100 dark:bg-slate-700' : '', theme === 'system' ? 'text-primary-700' : 'text-slate-700 dark:text-slate-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
+                            className={classNames(active ? 'bg-gray-100 dark:bg-gray-700' : '', theme === 'system' ? 'text-primary-700' : 'text-gray-700 dark:text-gray-400', 'flex gap-4 px-4 py-2 text-sm cursor-pointer')}
                           >
-                            <ComputerDesktopIcon className={classNames(theme === "system" ? "stroke-primary-700" : "stroke-slate-700 dark:stroke-slate-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
+                            <ComputerDesktopIcon className={classNames(theme === "system" ? "stroke-primary-700" : "stroke-gray-700 dark:stroke-gray-400", "h-5 w-5 stroke-2")} aria-hidden="true"/>
                             System
                           </a>
                         )}
