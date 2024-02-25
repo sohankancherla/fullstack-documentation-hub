158bb3933ab16fc70f53734eb5f8cd72e04d7af5
skancher <sohank@gmail.com>
2024-02-24 19:35:16 -0800

    Add dropdown menu for each nav item

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,31 @@
-import { Dialog, Menu, Transition } from '@headlessui/react'
+import { Dialog, Menu, Transition, Popover } from '@headlessui/react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import { Bars3Icon, SunIcon, MoonIcon, ComputerDesktopIcon, XMarkIcon} from '@heroicons/react/24/outline'
+import { Bars3Icon, SunIcon, MoonIcon, ComputerDesktopIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import {
+  ArrowPathIcon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+} from '@heroicons/react/24/outline'
 import React, { useEffect, useState, Fragment } from 'react';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+const solutions = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: "Your customers' data will be safe and secure", href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
 
 export default function NavigationBar({navigation, logo}) {
 
@@ -202,11 +222,59 @@ export default function NavigationBar({navigation, logo}) {
                 </div>
                 <div className="hidden lg:ml-10 lg:flex lg:space-x-8">
                   {navigation.map((item) => (
-                    <span
-                      className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm leading-6 font-semibold hover:text-primary-700 dark:text-slate-200 dark:hover:text-primary-700 cursor-pointer"
-                    >
-                      {item.name}
-                    </span>
+                    <Popover className="relative mt-4">
+                      <Popover.Button className="inline-flex items-center gap-x-0 text-sm font-semibold leading-6 text-slate-700 hover:text-primary-700 dark:text-slate-200 dark:hover:text-primary-700 focus:outline-none">
+                        <span
+                          className="px-1 pt-1 text-sm leading-6 font-semibold cursor-pointer"
+                        >
+                          {item.name}
+                        </span>
+                        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+                      </Popover.Button>
+                
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1"
+                      >
+                        <Popover.Panel className="absolute left-1/2 z-10 mt-6 flex w-screen max-w-max -translate-x-1/2 px-4">
+                          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white dark:bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                            <div className="p-4">
+                              {solutions.map((page) => (
+                                <div key={page.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+                                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                    <page.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                                  </div>
+                                  <div>
+                                    <a href={page.href} className="font-semibold text-gray-900">
+                                      {page.name}
+                                      <span className="absolute inset-0" />
+                                    </a>
+                                    <p className="mt-1 text-gray-600">{page.description}</p>
+                                  </div>
+                                </div>
+                              ))}
+                            </div>
+                            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                              {callsToAction.map((item) => (
+                                <a
+                                  key={item.name}
+                                  href={item.href}
+                                  className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
+                                >
+                                  <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                                  {item.name}
+                                </a>
+                              ))}
+                            </div>
+                          </div>
+                        </Popover.Panel>
+                      </Transition>
+                    </Popover>
                   ))}
                 </div>
               </div>
