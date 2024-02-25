f7550051fd127e64c670f90320140c3a74fb6d52
skancher <sohank@gmail.com>
2024-02-24 20:36:49 -0800

    Change dark mode colors for dropdown menus

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -23,8 +23,7 @@ const solutions = [
   { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
 ]
 const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+  { name: 'View More', href: '#', icon: PlayCircleIcon },
 ]
 
 export default function NavigationBar({navigation, logo}) {
@@ -241,35 +240,32 @@ export default function NavigationBar({navigation, logo}) {
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                       >
-                        <Popover.Panel className="absolute left-1/2 z-10 mt-6 flex w-screen max-w-max -translate-x-1/2 px-4">
-                          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white dark:bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                        <Popover.Panel className="absolute -left-32 2xl:left-1/2 z-10 mt-6 flex w-screen max-w-max 2xl:-translate-x-1/2 px-4">
+                          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white dark:bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 dark:ring-gray-950">
                             <div className="p-4">
                               {solutions.map((page) => (
-                                <div key={page.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-                                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <page.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                                <div key={page.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50 dark:hover:bg-slate-700">
+                                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 dark:bg-slate-700 group-hover:bg-white dark:group-hover:bg-slate-800">
+                                    <page.icon className="h-6 w-6 text-slate-700 dark:text-slate-400 group-hover:text-primary-700" aria-hidden="true" />
                                   </div>
                                   <div>
-                                    <a href={page.href} className="font-semibold text-gray-900">
+                                    <a href={page.href} className="font-semibold text-slate-900 dark:text-slate-200">
                                       {page.name}
                                       <span className="absolute inset-0" />
                                     </a>
-                                    <p className="mt-1 text-gray-600">{page.description}</p>
+                                    <p className="mt-1 text-slate-700 dark:text-slate-400">{page.description}</p>
                                   </div>
                                 </div>
                               ))}
                             </div>
-                            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                              {callsToAction.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
-                                >
-                                  <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-                                  {item.name}
-                                </a>
-                              ))}
+                            <div className="grid grid-cols-1 divide-x divide-gray-900/5 dark:divide-slate-800 bg-gray-50 dark:bg-slate-700">
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-slate-900 dark:text-slate-200 hover:bg-gray-100 dark:hover:bg-slate-600"
+                              >
+                                View More
+                              </a>
                             </div>
                           </div>
                         </Popover.Panel>
