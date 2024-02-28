9b250f2d2994b092d385f326a849e1ecb79572af
skancher <sohank@gmail.com>
2024-02-27 22:43:22 -0800

    Add alert that website is under construction

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Notification from '../components/Notification'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+
+const icon = <ExclamationTriangleIcon className="h-6 w-6 text-yellow-400" aria-hidden="true" />
+
+const title = "Under Construction";
+const body = "This website is currently under construction aiming to be ready by May 2024!";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Notification icon={icon} title={title} body={body} />
+      <div>HomePage</div>
+    </>
   )
 }
-
 export default HomePage
