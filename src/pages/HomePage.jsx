7a7c58d2bfe9cc9d498a93de0a9122c3b37d7f23
skancher <sohank@gmail.com>
2024-02-29 10:59:31 -0800

    Add dark mode colors for heading on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,13 +11,15 @@ const HomePage = () => {
   return (
     <>
       <Notification icon={icon} title={title} body={body} />
-      <div className="flex flex-col items-center justify-center relative py-32">
-        <div className='flex justify-center items-center gap-1  sm:gap-2 md:gap-2 lg:gap-3'>
-          <h1 className="text-3xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">Everything About</h1>
-          <h1 className="text-3xl tracking-tight text-primary-700 sm:text-5xl md:text-6xl">Full Stack,</h1>
+      <main>
+        <div className="flex flex-col items-center justify-center relative py-32">
+          <div className='flex justify-center items-center gap-1  sm:gap-2 md:gap-2 lg:gap-3'>
+            <h1 className="text-3xl tracking-tight text-gray-900 dark:text-gray-200 sm:text-5xl md:text-6xl">Everything About</h1>
+            <h1 className="text-3xl tracking-tight text-primary-700 sm:text-5xl md:text-6xl">Full Stack,</h1>
+          </div>
+          <h1 className="text-3xl tracking-tight text-gray-900 dark:text-gray-200 sm:text-5xl md:text-6xl">All in One Place</h1>
         </div>
-        <h1 className="text-3xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">All in One Place</h1>
-      </div>
+      </main>
     </>
   )
 }
