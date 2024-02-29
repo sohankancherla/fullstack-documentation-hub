92951f9f7c90fb009a5f2b2c2eccc6717a02168e
skancher <sohank@gmail.com>
2024-02-29 10:55:26 -0800

    Add header to the main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,13 @@ const HomePage = () => {
   return (
     <>
       <Notification icon={icon} title={title} body={body} />
-      <div>HomePage</div>
+      <div className="flex flex-col items-center justify-center relative py-32">
+        <div className='flex justify-center items-center gap-1  sm:gap-2 md:gap-2 lg:gap-3'>
+          <h1 className="text-3xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">Everything About</h1>
+          <h1 className="text-3xl tracking-tight text-primary-700 sm:text-5xl md:text-6xl">Full Stack,</h1>
+        </div>
+        <h1 className="text-3xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">All in One Place</h1>
+      </div>
     </>
   )
 }
